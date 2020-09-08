9725d6f402c1bfa7fa898469cf4d5402ada9d364
teboum <boumazzat@gmail.com>
2020-09-08 18:34:09 +0100

    links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -97,7 +97,7 @@ const Contact = ({ data, translate }) => {
               </div>
 
               <div>
-                <button type="submit" className="submit">
+                <button className="submit">
                   {translate && translate._submit}
                 </button>
                 <span id="image-loader">
